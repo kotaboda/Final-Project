b3e873560d4b0d9cfd7dd47506047a1dfc90ea97
Jeffrey Henderson <jayhenderson758@gmail.com>
2017-02-22 14:05:18 -0700

    added Viewable Interface

---
+++ src/viewInterface/Viewable.java
@@ -0,0 +1,17 @@
+package viewInterface;
+
+import battle.Battle;
+import itemSystem.Inventory;
+
+public interface Viewable {
+
+	
+	public void displayMainMenu();
+	public void displayPauseMenu();
+	public void displayBattleView(Battle b);
+	public void displayGeneralView();
+	public void displayInventoryView(Inventory inv);
+	public void displayCharacterManager();
+	public void displayLootManager();
+	
+}
