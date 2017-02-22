44e68d06c24d648f51b50156392bc5f7abb2fb57
Unknown <dakotajones.red@gmail.com>
2017-02-22 13:51:25 -0700

    Added player animation capability

--- src/character/Player.java
+++ src/character/Player.java
@@ -1,6 +1,25 @@
 package character;
 
+import characterInterfaces.Listener;
+import javafx.animation.Animation;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
 public class Player extends Character {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3349758486478712145L;
+
+	private final Image IMAGE = new Image("images/Player");
+	
+	private final int COLUMNS = 5;
+	private final int COUNT = 10;
+    private final int WIDTH = 32;
+    private final int HEIGHT = 32;
 
 	public Player(String name) {
 		super(name);
@@ -28,6 +47,32 @@ public class Player extends Character {
 		}
 		
 		return 15;
+	}
+	
+	public void move() {
+		 final ImageView imageView = new ImageView(IMAGE);
+	        imageView.setViewport(new Rectangle2D(WIDTH, HEIGHT, WIDTH, HEIGHT));
+
+	        final Animation animation = new SpriteAnimation(
+	                imageView,
+	                Duration.millis(1000),
+	                COUNT, COLUMNS,
+	                WIDTH, HEIGHT
+	        );
+	        animation.setCycleCount(1);
+	        animation.play();
+	}
+
+	@Override
+	public void addSubscriber(Listener<Character> sub) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeSubscriber(Listener<Character> sub) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
