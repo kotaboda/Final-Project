1b6d97da260345b96ee4972455636bd00e87a869
Jeffrey Henderson <jayhenderson758@gmail.com>
2017-02-26 12:16:40 -0700

    Added ability for player to move with WASD. Redraws the General Canvas

--- src/application/GameEngine.java
+++ src/application/GameEngine.java
@@ -5,7 +5,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import character.Player;
 import floors.Floor;
 import viewInterface.Viewable;
@@ -15,15 +14,15 @@ public class GameEngine {
 	private static Game game;
 
 	public static void run() {
-		//NOTE(andrew): This can be changed later, if it isn't done how we want it to be done
+		// NOTE(andrew): This can be changed later, if it isn't done how we want
+		// it to be done
 		int floorNum = game.getPlayer().getFloorNum();
 		Floor[] floors = game.getFloors();
 		view.setPlayerSummary(new Player("Jeffrey").getPlayerSummary());
-		view.displayGeneralView(/*floors[floorNum - 1]*/null);
+		view.displayGeneralView(/* floors[floorNum - 1] */null);
+
 
 	}
-	
-	
 
 	public static Game loadGame() {
 		Game g = new Game(new Player("Jeffrey"), null);
@@ -38,23 +37,23 @@ public class GameEngine {
 		}
 		return g;
 	}
-	
+
 	public static void saveGame(Game state) {
-		try(ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(Paths.get("src/saves/savedGame.neu")))){
+		try (ObjectOutputStream oos = new ObjectOutputStream(
+				Files.newOutputStream(Paths.get("src/saves/savedGame.neu")))) {
 			oos.writeObject(state);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void setView(Viewable newView) {
 		view = newView;
 	}
-	
-	public static void setGame(Game g){
+
+	public static void setGame(Game g) {
 		game = g;
 	}
 
-	
 }
--- src/application/GameGUI.java
+++ src/application/GameGUI.java
@@ -23,11 +23,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import models.Coordinates;
 import tiles.TileManager;
 import viewInterface.Viewable;
-
 
 public class GameGUI extends Application implements Viewable {
 	
@@ -141,12 +142,42 @@ public class GameGUI extends Application implements Viewable {
 
 	@Override
 	public void displayGeneralView(Floor currentFloor) {
-		// TODO Auto-generated method stub
 		FXMLLoader loader = new FXMLLoader();
 		
 		loader.setController(this);
 		try {
 			Parent parent = loader.load(Files.newInputStream(Paths.get("src/GeneralView.fxml")));
+			currentFloor.getPlayer().getCoordinates().setCoordinates(0, 0);
+			parent.setOnKeyPressed(new EventHandler<KeyEvent>(){
+
+				@Override
+				public void handle(KeyEvent event) {
+					KeyCode keyEvent = event.getCode();
+					Coordinates playerCoordinates = currentFloor.getPlayer().getCoordinates();
+					
+					switch(keyEvent){
+					case W:
+						playerCoordinates.setY(playerCoordinates.getY() - 1);
+						drawToGeneralCanvas(currentFloor);
+						break;
+					case S:
+						playerCoordinates.setY(playerCoordinates.getY() + 1);
+						drawToGeneralCanvas(currentFloor);
+
+						break;
+					case A:
+						playerCoordinates.setX(playerCoordinates.getX() - 1);
+						drawToGeneralCanvas(currentFloor);
+
+						break;
+					case D:
+						playerCoordinates.setX(playerCoordinates.getX() + 1);
+						drawToGeneralCanvas(currentFloor);
+
+						break;
+					}
+				}
+			});
 			menuButton.setOnAction(new EventHandler<ActionEvent>(){
 
 				@Override
@@ -159,10 +190,9 @@ public class GameGUI extends Application implements Viewable {
 			
 			playerName.setText(playerSummary.playerName);
 			
-			
 			//Drawing testing
 			GraphicsContext gc = canvas.getGraphicsContext2D();
-			WritableImage image = TileManager.getImageToDraw(currentFloor.getTiles(), new Coordinates(0, 5));
+			WritableImage image = TileManager.getImageToDraw(currentFloor.getTiles(), currentFloor.getPlayer().getCoordinates());
 			gc.drawImage(image, 0, 0, image.getWidth(), image.getHeight());
 			primaryStage.setScene(new Scene(parent));
 			primaryStage.show();
@@ -170,6 +200,13 @@ public class GameGUI extends Application implements Viewable {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	private void drawToGeneralCanvas(Floor currentFloor){
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+		WritableImage image = TileManager.getImageToDraw(currentFloor.getTiles(), currentFloor.getPlayer().getCoordinates());
+		gc.drawImage(image, 0, 0, image.getWidth(), image.getHeight());
 	}
 
 	@Override
@@ -196,5 +233,10 @@ public class GameGUI extends Application implements Viewable {
 	}
 
 
+	
+	
+	
+	
+	
 
 }
