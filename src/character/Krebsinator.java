f2a6b61877372341c88f7d286c7590809718bcd5
Jeffrey Henderson <jayhenderson758@gmail.com>
2017-03-10 22:35:43 -0700

    Added the PickOnYou ability used by Krebsinator to buff Mr. Krebs Wit, Also rewrote some of KrebsBattle to use this and log actions from enemies

--- src/character/Krebsinator.java
+++ src/character/Krebsinator.java
@@ -1,7 +1,5 @@
 package character;
 
-import interfaces.ability.Ability;
-
 public class Krebsinator extends Boss {
 	
 	/**
@@ -10,8 +8,6 @@ public class Krebsinator extends Boss {
 	private static final long serialVersionUID = 1846447040144482094L;
 
 	public Krebsinator() {
-		super("Krebsinator", 500, 76, (new Ability[] {
-				
-		}));
+		super("Krebsinator", 500, 76, new PickOnYou());
 	}
 }
