ae88efbb7a8545cd1c93685827fe1048f7520a3c
Jeffrey Henderson <jayhenderson758@gmail.com>
2017-03-10 22:11:23 -0700

    Added image initializers for when JoshKrebs and JerryPay are read in from a file (Need to add correct image path /they just use security guard images as placeholders/)

--- src/character/JerryPay.java
+++ src/character/JerryPay.java
@@ -1,10 +1,15 @@
 
 package character;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 import abilities.AddValue;
 import abilities.AssignATeamAssignment;
 import abilities.BoreToDeath;
 import abilities.ShowAPowerpoint;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.image.Image;
 
 public class JerryPay extends Boss {
 
@@ -16,6 +21,24 @@ public class JerryPay extends Boss {
 	public JerryPay() {
 		super("Jerry Pay", 1200, 73, new ShowAPowerpoint(), new AssignATeamAssignment(), new AddValue(),
 				new BoreToDeath());
+	}
+	
+	private void readObject(ObjectInputStream ois){
+		try {
+			ois.defaultReadObject();
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		hpProperty = new SimpleIntegerProperty(hitPoints);
+		maxHPProperty = new SimpleIntegerProperty(hitPoints);
+		energyProperty = new SimpleIntegerProperty(energy);
+		maxEnergyProperty = new SimpleIntegerProperty(maxEnergy);
+		
+		//TODO Replace the image paths
+		battleImage = new Image(getClass().getResourceAsStream("/images/guardicon.png"));
+		takeDamageAnimation = new Image(getClass().getResourceAsStream("/images/guarddamage.png"));
+		attackAnimation = new Image(getClass().getResourceAsStream("/images/guardattack.png"));
+		
 	}
 
 }
--- src/character/JoshKrebs.java
+++ src/character/JoshKrebs.java
@@ -1,8 +1,13 @@
 package character;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 import abilities.AssignFraculator;
 import abilities.Lecture;
 import abilities.PlayDivision;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.image.Image;
 
 public class JoshKrebs extends Boss {
 
@@ -13,5 +18,23 @@ public class JoshKrebs extends Boss {
 
 	public JoshKrebs() {
 		super("Josh Krebs", 1500, 74, new AssignFraculator(), new PlayDivision(), new Lecture());
+	}
+	
+	private void readObject(ObjectInputStream ois){
+		try {
+			ois.defaultReadObject();
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		hpProperty = new SimpleIntegerProperty(hitPoints);
+		maxHPProperty = new SimpleIntegerProperty(hitPoints);
+		energyProperty = new SimpleIntegerProperty(energy);
+		maxEnergyProperty = new SimpleIntegerProperty(maxEnergy);
+		
+		//TODO Replace image paths with correct images
+		battleImage = new Image(getClass().getResourceAsStream("/images/guardicon.png"));
+		takeDamageAnimation = new Image(getClass().getResourceAsStream("/images/guarddamage.png"));
+		attackAnimation = new Image(getClass().getResourceAsStream("/images/guardattack.png"));
+		
 	}
 }
